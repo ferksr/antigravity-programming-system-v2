7baafced35156917d76ab678e92a772c2bdc3fc8
Fernando Ariel Kuster <fernandoarielkuster@gmail.com>
2026-08-01 19:12:40 -0300

    test: enable globals for Vitest environment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
+    globals: true,
     environment: 'node',
     include: ['src/**/*.test.ts', 'src/**/*.test.tsx'],
   },
